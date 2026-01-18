a55a093f69893c70a5383b771195ab4aae700eca
JayMehta-21 <jaymehta466@gmail.com>
2026-01-18 23:06:32 +0530

    Add files via upload
    Generate a password checker that check user and their password

---
+++ Password_checker.py
@@ -0,0 +1,9 @@
+user=input("enter your User_Name : ")
+ps=input("enter your password : ")
+user_name="Jay Mehta"
+password="Peacemaker"
+
+if user==user_name and ps==password :
+    print("You are login in ")
+else:
+    print("Enter a valid user and password")
