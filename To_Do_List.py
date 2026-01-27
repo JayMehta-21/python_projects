601396aa043e799cc33597b2a8c7b5281717d780
JayMehta-21 <jaymehta466@gmail.com>
2026-01-27 22:49:00 +0530

    Update the file with add and view tasks

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,24 +1,55 @@
-def Add_task():
-    task=input("Enter your task :")
-    with open("task.txt","a") as tf:
-        tf.write(task)
+import os
 
-def View_task():
-    pass
-def Remove_task():
-    pass
-def main():
-    print("... Your to do list ...")
-    print('''     1) ADD TASK
-     2) VIEW TASK
-     3) REMOVE TASK''')
-    user_choice=int(input("Enter your choice(number only) : "))
-    if user_choice==1:
-        Add_task()
-    elif user_choice==2:
-        View_task()
-    elif user_choice==3:
-        Remove_task()
+file_name = "task_list.txt"
+
+def load_task():
+    if not os.path.exists(file_name):
+        return []
+    
+    with open(file_name,"r") as f:
+        tasks = f.read().splitlines()
+    return tasks
+
+def save_task(tasks):
+    with open(file_name, "w") as f:
+        for task in tasks:
+            f.write(task + "\n")
+
+def add_task(tasks):
+    task = input("Enter the task : ").strip()
+
+    if task:
+        tasks.append(task)
+        save_task(tasks)
+        print("Task Updated.")
     else:
-        print("Enter a Valid choice ")
-main()
+        print("Empty task not allowed.")
+
+def view(tasks):
+    if not tasks:
+        print("No task found!")
+        return
+    
+    for i , task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+
+def delete_task(tasks):
+    pass
+
+def main():
+    tasks = load_task()
+    
+    while True:
+        print("--------To-Do list--------")
+        print("1. Add Task \n2. View Task \n3. Delete Task")
+        user = int(input("Enter what you want you do : "))
+
+        if user ==  1:
+            add_task(tasks)
+        elif user == 2:
+            view(tasks)
+        elif user == 3:
+            delete_task(tasks)
+
+if __name__ == "__main__":
+    main()
