d441f4145f88e5723ba15ef216cb7239dda57ee0
JayMehta-21 <jaymehta466@gmail.com>
2026-01-30 22:42:31 +0530

    Update the database file with addition and deletion features

--- Database_To_Do.py
+++ Database_To_Do.py
@@ -1,15 +1,60 @@
 import sqlite3
-file="database_todo.db"
+
+file_name = "task_list.db"
+
+def get_connect():
+    return sqlite3.connect(file_name)
 
 def create_table():
-    conn=sqlite3.connect(file)
-    cursor=conn.cursor()
-    cursor.execute('''
-            CREATE TABLE IF NOT EXISTS TASKS(
-                ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                TASK TEXT NOT NULL
-                )
-    ''')
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS task(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   task TEXT NOT NULL,
+                   status TEXT NOT NULL
+                   )
+""")
+    
     conn.commit()
     conn.close()
-create_table()
+
+def add_task(task):
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO task(task, status) VALUES (?,?)
+""",(task,"Pending"))
+    
+    conn.commit()
+    conn.close()
+
+def get_task():
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT * FROM task""")
+    data =  cursor.fetchall()
+
+    conn.close()
+    return data
+
+def delete_task(task_id):
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM task WHERE id = ?
+""",(task_id, ))
+    
+    conn.commit()
+    deleted = cursor.rowcount
+    conn.close()
+
+    return deleted
