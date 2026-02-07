8a267f91f5635d103b5415d1b66f47fec9417e6a
JayMehta-21 <jaymehta466@gmail.com>
2026-02-07 23:13:23 +0530

    Add files via upload
    create some feature like set monthly budget, add expense and view budget options

---
+++ personal_expense_manager/reports.py
@@ -0,0 +1,11 @@
+from config import get_budget
+from database import get_total_expense
+
+def get_remaining_budget():
+
+    total_budget = get_budget()
+    total_expenses = get_total_expense()
+
+    remaining_budget = total_budget - total_expenses
+
+    return remaining_budget
