18a944dae0a9e4355a7d5b3820164e84ce68f611
JayMehta-21 <jaymehta466@gmail.com>
2026-02-09 11:48:58 +0530

    Update analytics file with the compare monthly budget feature

--- personal_expense_manager/analytics.py
+++ personal_expense_manager/analytics.py
@@ -41,7 +41,7 @@ def get_top_category(month):
 
     cursor.execute("""
         SELECT category, SUM(amount) AS total FROM expenses WHERE date LIKE ? GROUP BY category 
-                   ORDER BY total DESC LIMIT 1
+                   ORDER BY total DESC LIMIT 3
 """)
 
     top = cursor.fetchone()
@@ -50,3 +50,32 @@ def get_top_category(month):
     if top:
         return top[0], top[1]
     return None
+
+def compare_month(month):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    split_string = month.split(sep="-")
+    year = int(split_string[0])
+    mon = int(split_string[1])
+
+    if mon > 1:
+        mon = mon - 1
+    elif mon == 1:
+        year = year - 1
+        mon = 12
+    new_year = str(year)
+    new_month = str(mon)
+
+    pattern = f"{new_year}-0{new_month}%"
+
+    cursor.execute("""
+        SELECT SUM(amount) FROM expenses WHERE date LIKE ?
+""",(pattern, ))
+    
+    row = cursor.fetchone()
+    prev_total = row[0] if row[0] is not None else 0
+
+    conn.close()
+    return  prev_total
